76080e4ade3e1fbb7d41c85c7a425fe2a0096be8
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-05-12 00:06:54 +0545

    readme.ml file has been added

---
+++ readme.ml
@@ -0,0 +1,5 @@
+### to run this app:
+1. run `python3.8 pip install requirements.txt`
+2. cd to Blockchain
+3. **if using virtualenv then activate it, otherwise skit this step**
+4. Run `python Node1.py`
