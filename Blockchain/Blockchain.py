8bb15a4c6577a8c8436987b492fa7d8f9f28ee0d
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-05-11 23:48:28 +0545

    simple template for Node1 has been created

---
+++ Blockchain/Blockchain.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+class Blockchain:
+
+    def __init__(self):
+            self.transactions = []
+            self.chain = []
+
+
+# instanciating block chain
+blockchain = Blockchain()
+
+#instantiating node
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('./index.html')
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument('-p',
+                        '--port',
+                        default=5001,
+                        help = 'port to listen to')
+    args = parser.parse_args()
+    port = args.port
+
+    app.run(host='127.0.0.1', port=port, debug=True)
