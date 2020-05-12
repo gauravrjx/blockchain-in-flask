b86c75f26556261431f308dee46f9b49484ea532
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-05-12 13:54:54 +0545

    custom npm start and npm start:blockchain defined

--- readme.ml
+++ readme.ml
@@ -6,3 +6,11 @@
 5. Run `python Blockchain.py -p 5002`
 6. Run `python Blockchain_client -p 8001`
 7. Run `python Blockchain_client -p 8002`
+
+
+## to build tailwindcss
+1. just run `npm start` to run for **Blockchain_client.py** and for **Blockchain.py** run `npm start:blockchain`
+note that, everytime we modify css, we gotta run it <br />
+
+**why command `npm start` works?**
+answer lies in *script of package.json(build by running `npm init`)*
