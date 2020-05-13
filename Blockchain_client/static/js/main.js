499ade9eab9a4216de3f16d296def5ae48522032
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-05-13 15:57:41 +0545

    nav item isue with resizing has been fixed

--- Blockchain_client/static/js/main.js
+++ Blockchain_client/static/js/main.js
@@ -5,7 +5,11 @@ const menuIcon = document.querySelector(".nav-btn");
 // Event Listener
 menuIcon.addEventListener('click', displayMenu);
 
+
+
 // Functions
+
+// to display nav items in small screen
 function displayMenu(event){
     // console.log(menuDiv.classList[0]);
     switch(menuDiv.classList[0]){
@@ -15,5 +19,14 @@ function displayMenu(event){
         case "nav-menu":
             menuDiv.className="toggle-menu";
             break
+    }
+}
+
+
+// when navicons are shown and windows are maximized nav items should be nav-menu
+window.onresize = () => {
+    if(window.innerWidth > 768 ){
+        // console.log(window.innerWidth)
+        menuDiv.className="nav-menu"
     }
 }
