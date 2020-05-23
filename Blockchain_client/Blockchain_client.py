ae13f52329890672a9a6fc38b8062afa24b945c3
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-05-23 16:28:48 +0545

    private key and public key functionality fully implemented

--- Blockchain_client/Blockchain_client.py
+++ Blockchain_client/Blockchain_client.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify
+from Crypto.PublicKey import RSA
+from Crypto import Random as crypto_random
+import binascii
 
 class Transaction:
-
-    def __init__(self, sender_address, sender_private_key,
-                 recipient_address, value):
+    def __init__(self, sender_address, sender_private_key, recipient_address,
+                 value):
         self.sender_address = sender_address
         self.sender_private_key = sender_private_key
         self.value = value
@@ -13,32 +14,41 @@ class Transaction:
 #instantiating node~
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('/index.html')
+
 
 @app.route('/make/transaction')
 def make_transation():
     return render_template('./make_transaction.html')
 
+
 @app.route('/view/transaction')
 def view_transaction():
     return render_template('./view_transaction.html')
 
+
 @app.route('/wallet/new')
 def wallet():
-    return ''
+    random_gen = crypto_random.new().read
+    private_key = RSA.generate(1024, random_gen)
+    public_key = private_key.publickey()
 
+    response = {
+        'private_key': binascii.hexlify(private_key.export_key(format('DER'))).decode('ascii'),
+        'public_key': binascii.hexlify(public_key.export_key(format('DER'))).decode('ascii')
+    }
+
+    return jsonify(response), 200
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
 
     parser = ArgumentParser()
-    parser.add_argument('-p',
-                        '--port',
-                        default=8001,
-                        help = 'port to listen to')
+    parser.add_argument('-p', '--port', default=8001, help='port to listen to')
     args = parser.parse_args()
     port = args.port
 
--- Blockchain_client/static/js/main.js
+++ Blockchain_client/static/js/main.js
@@ -83,8 +83,9 @@ function genKeyAjax(event){
     const xhr = new XMLHttpRequest();
     xhr.onload = function(){
         if(this.status === 200){
-            textAreaKeis[0].innerHTML = this.response["public_key"]
-            textAreaKeis[1].innerHTML = this.response["private_key"]
+            jsonResponse = JSON.parse(this.response)
+            textAreaKeis[0].innerHTML = jsonResponse["public_key"]
+            textAreaKeis[1].innerHTML = jsonResponse["private_key"]
             alert("IMPORTANT\n1. Save both keyies as they are not recoverable\n2. Never Share PRIVATE key with anybody.");
         }else{
             alert("An error occured");
