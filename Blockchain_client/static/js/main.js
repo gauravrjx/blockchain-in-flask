7fea386f8a6591ec954db5238301cdf66e530746
gaurav-zaiswal <gauravzaiswal@gmail.com>
2020-05-25 17:18:17 +0545

    readme file modified

--- Blockchain_client/static/js/main.js
+++ Blockchain_client/static/js/main.js
@@ -1,42 +1,43 @@
 // selectors
-    //index.html -> blockchain_client
+//index.html -> blockchain_client
+window.onload = function(){
 const menuDiv = document.querySelector(".nav-menu");
 const menuIcon = document.querySelector(".nav-btn");
 const copyBtns = document.getElementsByClassName("copy-btn");
 const textAreaKeis = document.getElementsByClassName("txt-area-key");
 const walletBtn = document.querySelector(".wallet-btn");
-    //make_transaction.html -> blockchain_client
-const generateForm = document.querySelector("#form-f-transaction");
-const tranPubKey = document.querySelector(".pub-key-f-tra");
-const tranPriKey = document.querySelector(".pri-key-f-tra");
+//make_transaction.html -> blockchain_client
+const tranForm = document.querySelector("#tran-form");
+const senderPubKey = document.querySelector(".pub-key-f-tra");
+const senderPriKey = document.querySelector(".pri-key-f-tra");
 const rcvrPubKey = document.querySelector(".rcvr-pub-key-f-trac");
 const amtToSend = document.querySelector(".amt-f-trac");
-const tranBtn = document.querySelector(".transaction-btn");
-console.log(34567);
+const tranBtn = document.querySelector("tran-form-btn");
+// console.log(tranForm);
 
 // Event Listener
-    //index.html -> blockchain_client
+//index.html -> blockchain_client
 menuIcon.addEventListener('click', displayMenu);
 copyBtns[0].addEventListener('click', copyTextToClipboard);
 copyBtns[1].addEventListener('click', copyTextToClipboard);
 walletBtn.addEventListener('click', genKeyAjax);
-    //make_transaction.html -> blockchain_client
-generateForm.addEventListener('submit', makeTranAjax);
-// tranBtn.addEventListener('click', makeTranAjax);
+//make_transaction.html -> blockchain_client
+tranForm.addEventListener('submit', tranAjax);
+// tranBtn.addEventListener('click', tranAjax);
 
 
 
 // Functions
 
 // to display nav items in small screen
-function displayMenu(event){
+function displayMenu(event) {
     // console.log(menuDiv.classList[0]);
-    switch(menuDiv.classList[0]){
+    switch (menuDiv.classList[0]) {
         case "toggle-menu":
-            menuDiv.className="nav-menu";
+            menuDiv.className = "nav-menu";
             break
         case "nav-menu":
-            menuDiv.className="toggle-menu";
+            menuDiv.className = "toggle-menu";
             break
     }
 }
@@ -44,61 +45,61 @@ function displayMenu(event){
 
 // NAV-BAR when navicons are shown and windows are maximized nav items should be nav-menu
 window.onresize = () => {
-    if(window.innerWidth > 768 ){
+    if (window.innerWidth > 768) {
         // console.log(window.innerWidth)
-        menuDiv.className="nav-menu"
+        menuDiv.className = "nav-menu"
     }
 }
 
 
 // to copy public-private key from blockchain_client -> index.html
-function copyTextToClipboard(event){
+function copyTextToClipboard(event) {
     // console.log(event.target.getAttribute("id"));
     targetId = event.target;
-    if(targetId.getAttribute("id") === "public-copy-btn"){
+    if (targetId.getAttribute("id") === "public-copy-btn") {
         textAreaKeis[0].focus();
         textAreaKeis[0].select();
-    
+
         try {
-        var successful = document.execCommand('copy');
-            if(successful){
-                copyBtns[0].innerText = "Copied";  
+            var successful = document.execCommand('copy');
+            if (successful) {
+                copyBtns[0].innerText = "Copied";
             }
         } catch (err) {
-        alert("Oops, copy button didn't work, Please copy manually");
-        }finally{
+            alert("Oops, copy button didn't work, Please copy manually");
+        } finally {
             targetId.setAttribute("id", "public-copied-btn")
         }
-    }else{
+    } else {
         textAreaKeis[1].focus();
         textAreaKeis[1].select();
-    
+
         try {
-        var successful = document.execCommand('copy');
-            if(successful){
-                copyBtns[1].innerText = "Copied"; 
+            var successful = document.execCommand('copy');
+            if (successful) {
+                copyBtns[1].innerText = "Copied";
 
             }
         } catch (err) {
-        alert("Oops, copy button didn't work, Please copy manually");
-        }finally{
+            alert("Oops, copy button didn't work, Please copy manually");
+        } finally {
             targetId.setAttribute("id", "private-copied-btn")
         }
     }
 }
 
 // to generate key
-function genKeyAjax(event){
+function genKeyAjax(event) {
     event.preventDefault();
 
     const xhr = new XMLHttpRequest();
-    xhr.onload = function(){
-        if(this.status === 200){
+    xhr.onload = function () {
+        if (this.status === 200) {
             jsonResponse = JSON.parse(this.response)
             textAreaKeis[0].innerHTML = jsonResponse["public_key"]
             textAreaKeis[1].innerHTML = jsonResponse["private_key"]
             alert("IMPORTANT\n1. Save both keyies as they are not recoverable\n2. Never Share PRIVATE key with anybody.");
-        }else{
+        } else {
             alert("An error occured");
         }
     }
@@ -108,21 +109,31 @@ function genKeyAjax(event){
 }
 
 // make transaction
-function makeTranAjax(event){
-    event.preventDefault();
-    console.log(event);
-    
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function(){
-        if(this.status === 200){
-            jsonResponse = JSON.parse(this.response);
-            
-        }else{
+function tranAjax(e) {
+    e.preventDefault();
+
+    const xrh = new XMLHttpRequest();
+
+    xrh.onload = function () {
+        if (this.status === 200) {
+            console.log('form has been submitted!')
+
+        } else {
             console.error();
             alert("An error occured");
         }
     }
 
-    xhr.open("GET", "/generate/transaction", true);
-    xhr.send();
+    xrh.open("POST", "/generate/transaction", true);
+    // to send POST request
+    xrh.setRequestHeader("Content-type", "application/json");
+
+    var data = JSON.stringify({
+        'sender_public_key': senderPubKey.value,
+        'sender_private_key': senderPriKey.value,
+        'recipient_public_key': rcvrPubKey.value,
+        'amount': amtToSend.value
+    });
+    xrh.send(data);
+}
 }
--- readme.md
+++ readme.md
@@ -1,17 +1,10 @@
 ### to run this app:
 1. run `python3.8 pip install requirements.txt`
 2. cd to Blockchain
-3. **if using virtualenv then activate it, otherwise skit this step**
-    run `source ./env/bin/activate`
 4. Run `python ./Blackchain/Blockchain.py -p 5001`
-5. Run `python Blockchain.py -p 5002`
-6. Run `python Blockchain_client/Blockchain_client.py -p 8001`
-7. Run `python Blockchain_client -p 8002`
+5. Run `python ./Blackchain/Blockchain.py -p 5002`
+6. Run `python ./Blockchain_client/Blockchain_client.py -p 8001`
+7. Run `python ./Blockchain_client/Blockchain_client.py -p 8002`
 
 
-## to build tailwindcss
-1. just run `npm start` to run for **Blockchain_client.py** and for **Blockchain.py** run `npm start:blockchain`
-note that, everytime we modify css, we gotta run it <br />
 
-**why command `npm start` works?**
-answer lies in *script of package.json(build by running `npm init`)*
